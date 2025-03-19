b5aeff212244c49ead07910f7a042c96f9d9516e
Jean Michel Da Silva <jeanms2305@gmail.com>
2025-03-19 20:37:48 -0300

    Mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -24,16 +25,30 @@ class MainActivity : AppCompatActivity() {
 
         btncalcular.setOnClickListener {
 
-            val peso:Float = edtpeso.text.toString().toFloat()
-            val altura:Float = edtaltura.text.toString().toFloat()
+            val pesoStr:String = edtpeso.text.toString()
+            val alturaStr:String = edtaltura.text.toString()
 
-            val altQ2 = altura * altura
-            val resultado = peso / altQ2
+            if(pesoStr == "" || alturaStr == "") {
+             // Mostrar mensagem para o usuario
 
-            println ( "Jean " + resultado )
+                Snackbar
+                    .make(
+                        edtpeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            } else {
+                val peso: Float = pesoStr.toFloat()
+                val altura: Float = alturaStr.toFloat()
+
+                val altQ2 = altura * altura
+                val resultado = peso / altQ2
+
+
+                println ( "Jean " + resultado )
+            }
         }
-
-
-
     }
 }
